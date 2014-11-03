38d9091ea037c33fb38927813c023dbe73af982d
Tony McCallie <tony@threeleaf.net>
2014-11-03 12:06:15 -0600

    look back further

--- app/Controller/ClaimsController.php
+++ app/Controller/ClaimsController.php
@@ -246,7 +246,7 @@ class ClaimsController extends AppController {
 		$HttpSocket = new HttpSocket();
 		
 		//$startDate = '-2 hour';
-		$startDate = '-2 days';
+		$startDate = '-30 days';
 		
 		$data = array(
 			'ACID' => '33152',
